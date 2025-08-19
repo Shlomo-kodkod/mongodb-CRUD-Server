4c445de9b62bc1162c0d18b34a6fcb045e74ba7a
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-19 17:32:32 +0300

    Add Soldier class with initialization and string representation methods

---
+++ service/model.py
@@ -0,0 +1,18 @@
+
+
+class Soldier:
+
+    id_number = 0
+
+    def __init__(self, soldier_id, first_name, last_name):
+        self.soldier_id = soldier_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.rank = Soldier.id_number + 1
+
+
+    def __repr__(self):
+        return f"Soldier({self.soldier_id}, {self.first_name}, {self.last_name}, {self.rank})"
+    
+    def __str__(self):
+        return f"Soldier ID: {self.soldier_id}, Name: {self.first_name} {self.last_name}, Rank: {self.rank}"
