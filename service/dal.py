fe9531e8d4d19c57793555927834fa201bc9ba9f
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-19 17:59:38 +0300

    Enhance DAL class with error handling and logging for MongoDB connection and data retrieval

--- service/dal.py
+++ service/dal.py
@@ -1,11 +1,35 @@
 from pymongo import MongoClient
+import logging
 from service import config  
+
+logger = logging.getLogger(__name__)
 
 class DAL:
 
     @staticmethod
     def connect(self):
-        client = MongoClient(host=config.host, port=config.port, username=config.username, password=config.password)
-        db = client[config.db]
-        return db
+        try:
+            client = MongoClient(host=config.host, port=config.port, username=config.username, password=config.password)
+            db = client[config.db]
+            logger.info(f"Connected to MongoDB at {config.host}:{config.port}")
+            return db
+        except Exception as e:
+            logger.error(f"Failed to connect to MongoDB: {e}")
+            raise e
+    
+    @staticmethod
+    def get_collection(collection_name=config.collection):
+        try:
+            db = DAL.connect()
+            collection = db[collection_name]
+            data = list(collection.find({}, {"_id": 0}))  
+            logger.info(f"Data loaded successfully from {collection.name} collection.")
+            return data
+        except Exception as e:
+            logger.error(f"Failed to retrieve collection {collection_name}: {e}")
+            raise e
+        
+        
+        
+
     
