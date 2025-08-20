eec943be48c5d911c88cfecc27be4110b2ab8ffd
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-20 13:48:54 +0300

    feat: add FastAPI deployment and service configurations for Kubernetes

---
+++ scripts/commands.bat
@@ -0,0 +1,14 @@
+oc apply -f mongodb-secret.yaml
+oc apply -f mongo-pvc.yaml
+oc apply -f mongodb-deployment.yaml
+oc apply -f mongo-service.yaml
+
+
+docker build -t kodkod1docker/mongo-crud-server:v1 .
+docker push kodkod1docker/mongo-crud-server:v1
+
+
+oc apply -f fastapi-deployment.yaml
+oc apply -f fastapi-service.yaml
+
+oc expose service fastapi-server --port=8083 --target-port=8083 --name=fastapi-server
